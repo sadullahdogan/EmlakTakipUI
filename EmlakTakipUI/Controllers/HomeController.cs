ebe097ff2de79508f4c2ba0fc3fb5b7c2b7a6c3e
Sadullah <sadullah.dogan@yandex.com>
2020-01-29 18:19:48 +0300

    .

--- EmlakTakipUI/Controllers/HomeController.cs
+++ EmlakTakipUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseAccessLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,18 @@ namespace EmlakTakipUI.Controllers
         }
         public ActionResult AboutUs() {
             var model = db.Companies.Find(1);
+            if (model == null) {
+                model = new Company();
+                model.Adress = "CompanyAdress";
+                model.EMail = "company.company@company.com";
+                model.Fax = "11111111111";
+                model.Logo = "logo.png";
+                model.ManagerName = "Manager";
+                model.ManagerSurname = "ManagerLAstname";
+                model.Phone = "222222222222";
+                db.Companies.Add(model);
+                db.SaveChanges();
+            }
             return PartialView(model);
         }
     }
